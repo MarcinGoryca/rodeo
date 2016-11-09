80ddad70ff9f7b1d056a6b68d5f1aa145581a0b6
Marcin <marcin.goryca@gmail.com>
2016-11-10 00:06:14 +0100

    Up #10 Nov, 2016 by MG

---
+++ resources/resource.h
@@ -0,0 +1,24 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define GraphicsSettingsDialog                  103
+#define IDD_DIALOG1                             104
+#define iddOpenfile                             106
+#define IDC_SLIDER1                             1001
+#define idc800                                  1011
+#define idc1024                                 1012
+#define idc1280                                 1013
+#define idc1360                                 1014
+#define idc1600                                 1015
+#define idc24bit                                1016
+#define idc32bit                                1017
+#define idc60mhz                                1018
+#define idc75mhz                                1019
+#define idcFullscreen                           1020
+#define IDC_SLIDER2                             1021
+#define idcSave                                 1021
+#define IDC_SLIDER3                             1022
+#define IDC_COMBO1                              1023
+#define IDC_LIST1                               1024
+#define IDD_ABOUT								1025
